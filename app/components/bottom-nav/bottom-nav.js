e7c1a516cc702dfdf172c41fa587ffef7702f676
dorothy-yl <dorothy.luluya@gmail.com>
2025-12-16 18:34:20 +0800

    实现个人中心页面

--- app/components/bottom-nav/bottom-nav.js
+++ app/components/bottom-nav/bottom-nav.js
@@ -7,7 +7,25 @@ Component({
   },
   methods: {
     onSwitchTab(e) {
+      console.log('onSwitchTab', e);
+      
       const { path, tab } = e.currentTarget.dataset;
+      if (tab=='user') {
+        const {
+          query: { deviceId }
+        } = ty.getLaunchOptionsSync();
+         
+        ty.device.openDeviceDetailPage({
+          deviceId,
+          success: () => {
+            console.log('openDeviceDetailPage success');
+          },
+          fail: (error) => {
+            console.log('openDeviceDetailPage fail', error);
+          }
+        });
+        return
+      }
       if (tab === this.data.activeTab) return;
       
       ty.redirectTo({
